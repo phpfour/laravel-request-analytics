45cd6cdcfbdef249c1a2f50c082215b20b0203dd
OmarFaruk-0x01 <OmarFaruk-0x01@users.noreply.github.com>
2024-03-05 16:13:13 +0000

    Fix styling

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use MeShaon\RequestAnalytics\Controllers\RequestAnalyticsController;
 
 Route::get('/analytics', [RequestAnalyticsController::class, 'show'])->name('request.analytics');
--- src/Controllers/RequestAnalyticsController.php
+++ src/Controllers/RequestAnalyticsController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace MeShaon\RequestAnalytics\Controllers;
+
 use Illuminate\Routing\Controller as BaseController;
 
 class RequestAnalyticsController extends BaseController
@@ -8,6 +9,7 @@ class RequestAnalyticsController extends BaseController
     public function show()
     {
         $chartData = $this->getChartData();
+
         return view('request-analytics::analytics', [
             'browsers' => $this->getBrowsers(),
             'operatingSystems' => $this->getOperatingSystems(),
@@ -17,7 +19,7 @@ class RequestAnalyticsController extends BaseController
             'labels' => $chartData['labels'],
             'datasets' => $chartData['datasets'],
             'average' => $this->getAverage(),
-            'countries' => $this->getCountries()
+            'countries' => $this->getCountries(),
         ]);
     }
 
@@ -25,30 +27,30 @@ class RequestAnalyticsController extends BaseController
     {
         return [
             [
-                "name" => "United States",
-                "visitorCount" => 100,
-                "percentage" => 50,
-                "code" => "us"
+                'name' => 'United States',
+                'visitorCount' => 100,
+                'percentage' => 50,
+                'code' => 'us',
 
             ],
             [
-                "name" => "Canada",
-                "visitorCount" => 100,
-                "percentage" => 50,
-                "code" => "ca"
+                'name' => 'Canada',
+                'visitorCount' => 100,
+                'percentage' => 50,
+                'code' => 'ca',
             ],
             [
-                "name" => "United Kingdom",
-                "visitorCount" => 100,
-                "percentage" => 50,
-                "code" => "gb"
+                'name' => 'United Kingdom',
+                'visitorCount' => 100,
+                'percentage' => 50,
+                'code' => 'gb',
             ],
             [
-                "name" => "Australia",
-                "visitorCount" => 100,
-                "percentage" => 50,
-                "code" => "au"
-            ]
+                'name' => 'Australia',
+                'visitorCount' => 100,
+                'percentage' => 50,
+                'code' => 'au',
+            ],
         ];
     }
 
@@ -56,15 +58,15 @@ class RequestAnalyticsController extends BaseController
     {
         return [
             [
-                "browser" => "Chrome",
-                "visitorCount" => 100,
-                "percentage" => 50
+                'browser' => 'Chrome',
+                'visitorCount' => 100,
+                'percentage' => 50,
             ],
             [
-                "browser" => "Firefox",
-                "visitorCount" => 100,
-                "percentage" => 50
-            ]
+                'browser' => 'Firefox',
+                'visitorCount' => 100,
+                'percentage' => 50,
+            ],
         ];
     }
 
@@ -72,20 +74,20 @@ class RequestAnalyticsController extends BaseController
     {
         return [
             [
-                "name" => "Windows",
-                "visitorCount" => 100,
-                "percentage" => 50
+                'name' => 'Windows',
+                'visitorCount' => 100,
+                'percentage' => 50,
             ],
             [
-                "name" => "Linux",
-                "visitorCount" => 100,
-                "percentage" => 50
+                'name' => 'Linux',
+                'visitorCount' => 100,
+                'percentage' => 50,
             ],
             [
-                "name" => "MacOS",
-                "visitorCount" => 100,
-                "percentage" => 50
-            ]
+                'name' => 'MacOS',
+                'visitorCount' => 100,
+                'percentage' => 50,
+            ],
         ];
     }
 
@@ -93,19 +95,19 @@ class RequestAnalyticsController extends BaseController
     {
         return [
             [
-                "name" => "Mobile",
-                "visitorCount" => 100,
-                "percentage" => 50
+                'name' => 'Mobile',
+                'visitorCount' => 100,
+                'percentage' => 50,
             ],
             [
-                "name" => "Tablet",
-                "visitorCount" => 100,
-                "percentage" => 50
+                'name' => 'Tablet',
+                'visitorCount' => 100,
+                'percentage' => 50,
             ],
             [
-                "name" => "Desktop",
-                "visitorCount" => 100,
-                "percentage" => 50
+                'name' => 'Desktop',
+                'visitorCount' => 100,
+                'percentage' => 50,
             ],
         ];
     }
@@ -114,20 +116,20 @@ class RequestAnalyticsController extends BaseController
     {
         return [
             [
-                "path" => "/",
-                "visitorCount" => 100,
-                "percentage" => 50
+                'path' => '/',
+                'visitorCount' => 100,
+                'percentage' => 50,
             ],
             [
-                "path" => "/about",
-                "visitorCount" => 100,
-                "percentage" => 50
+                'path' => '/about',
+                'visitorCount' => 100,
+                'percentage' => 50,
             ],
             [
-                "path" => "/contact",
-                "visitorCount" => 100,
-                "percentage" => 50
-            ]
+                'path' => '/contact',
+                'visitorCount' => 100,
+                'percentage' => 50,
+            ],
         ];
     }
 
@@ -135,20 +137,20 @@ class RequestAnalyticsController extends BaseController
     {
         return [
             [
-                "domain" => "google.com",
-                "visitorCount" => 100,
-                "percentage" => 50
+                'domain' => 'google.com',
+                'visitorCount' => 100,
+                'percentage' => 50,
             ],
             [
-                "domain" => "facebook.com",
-                "visitorCount" => 100,
-                "percentage" => 50
+                'domain' => 'facebook.com',
+                'visitorCount' => 100,
+                'percentage' => 50,
             ],
             [
-                "domain" => "twitter.com",
-                "visitorCount" => 100,
-                "percentage" => 50
-            ]
+                'domain' => 'twitter.com',
+                'visitorCount' => 100,
+                'percentage' => 50,
+            ],
         ];
     }
 
@@ -157,30 +159,31 @@ class RequestAnalyticsController extends BaseController
         $labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October'];
         $datasets = [
             [
-                "label" => "Views",
-                "data" => [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-                "backgroundColor" => "rgba(255, 99, 132, 0.2)",
-                "borderColor" => "rgba(255, 99, 132, 1)",
-                "borderWidth" => 1
+                'label' => 'Views',
+                'data' => [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+                'backgroundColor' => 'rgba(255, 99, 132, 0.2)',
+                'borderColor' => 'rgba(255, 99, 132, 1)',
+                'borderWidth' => 1,
             ],
             [
-                "label" => "Visitors",
-                "data" => [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
-                "backgroundColor" => "rgba(54, 162, 235, 0.2)",
-                "borderColor" => "rgba(54, 162, 235, 1)",
-                "borderWidth" => 1
-            ]
+                'label' => 'Visitors',
+                'data' => [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
+                'backgroundColor' => 'rgba(54, 162, 235, 0.2)',
+                'borderColor' => 'rgba(54, 162, 235, 1)',
+                'borderWidth' => 1,
+            ],
         ];
+
         return compact('labels', 'datasets');
     }
 
     private function getAverage()
     {
         return [
-            "views" => 100,
-            "visitors" => 100,
-            "bounce-rate" => 100,
-            "average-visit-time" => 100
+            'views' => 100,
+            'visitors' => 100,
+            'bounce-rate' => 100,
+            'average-visit-time' => 100,
         ];
     }
 }
