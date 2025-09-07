61e0635d51b7c07f8bea4894dba9bb726850e274
Ahmed shamim <shaon.cse81@gmail.com>
2025-09-07 19:17:24 +0600

    fix: filtering

--- resources/views/components/core/button.blade.php
+++ resources/views/components/core/button.blade.php
@@ -1,8 +1,9 @@
 @props([
     'color' => 'primary',
+    'type' => 'button',
 ])
 
-<button type="button"
+<button type="{{ $type }}"
     @class([
     "inline-flex items-center justify-center text-sm font-medium tracking-wide transition-colors duration-100 rounded-md px-5 py-2.5 h-10",
         'bg-blue-100 text-blue-500 hover:bg-blue-200' => $color === 'primary',
